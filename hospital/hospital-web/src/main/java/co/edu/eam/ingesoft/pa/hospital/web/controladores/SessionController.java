a4102ff6872dcecdb1f49a1cb4a2172e369d3b36
julian1117 <julian5296@gmail.com>
2017-05-14 00:09:57 -0500

    Orgenizacion estructura web por carpetas

--- hospital/hospital-web/src/main/java/co/edu/eam/ingesoft/pa/hospital/web/controladores/SessionController.java
+++ hospital/hospital-web/src/main/java/co/edu/eam/ingesoft/pa/hospital/web/controladores/SessionController.java
@@ -16,15 +16,13 @@ import co.edu.eam.ingesoft.avanzada.persistencia.entidades.Roll;
 import co.edu.eam.ingesoft.avanzada.persistencia.entidades.Usuario;
 import co.edu.eam.ingesoft.pa.negocio.beans.SeguridadEJB;
 
-
-
 @Named("sessionController")
 @SessionScoped
 public class SessionController implements Serializable {
 
 	private String usuario;
 	private String contrasena;
-	
+
 	private List<Acceso> accesos;
 
 	private List<Roll> roles;
@@ -54,8 +52,6 @@ public class SessionController implements Serializable {
 	public void setUse(Usuario use) {
 		this.use = use;
 	}
-	
-	
 
 	public List<Acceso> getAccesos() {
 		return accesos;
@@ -73,8 +69,6 @@ public class SessionController implements Serializable {
 		this.roles = roles;
 	}
 
-
-
 	@EJB
 	private SeguridadEJB seguridadEjb;
 
@@ -83,22 +77,46 @@ public class SessionController implements Serializable {
 	 */
 	public String login() {
 
-	
 		Usuario useBuscar = seguridadEjb.buscar(usuario);
 
 		if (useBuscar != null) {
-			
+
 			if (useBuscar.getContrasenia().equals(contrasena)) {
-				use=useBuscar;
-				
-				Faces.setSessionAttribute("usuario",use);
-				Messages.addGlobalInfo("Usuario existe");
-				
-				roles = seguridadEjb.listaRoles(useBuscar.getIdPersona().getRoll().getId());
-				accesos = seguridadEjb.listaAcc(useBuscar.getIdPersona().getRoll().getId());
-				
-				
-				return "/paginas/seguro/Citas.xhtml?faces-redirect=true";
+				use = useBuscar;
+				if (use.getIdPersona().getRoll().getId() == 1) {
+					Faces.setSessionAttribute("usuario", use);
+					Messages.addGlobalInfo("Usuario existe");
+
+					roles = seguridadEjb.listaRoles(useBuscar.getIdPersona().getRoll().getId());
+					accesos = seguridadEjb.listaAcc(useBuscar.getIdPersona().getRoll().getId());
+
+					return "/paginas/seguro/Adm/Citas.xhtml?faces-redirect=true";
+					
+				} else if (use.getIdPersona().getRoll().getId() == 2) {
+					Faces.setSessionAttribute("usuario", use);
+					Messages.addGlobalInfo("Usuario existe");
+
+					roles = seguridadEjb.listaRoles(useBuscar.getIdPersona().getRoll().getId());
+					accesos = seguridadEjb.listaAcc(useBuscar.getIdPersona().getRoll().getId());
+
+					return "/paginas/seguro/paciente/#.xhtml?faces-redirect=true";
+				}else if (use.getIdPersona().getRoll().getId() == 3) {
+					Faces.setSessionAttribute("usuario", use);
+					Messages.addGlobalInfo("Usuario existe");
+
+					roles = seguridadEjb.listaRoles(useBuscar.getIdPersona().getRoll().getId());
+					accesos = seguridadEjb.listaAcc(useBuscar.getIdPersona().getRoll().getId());
+
+					return "/paginas/seguro/Medico/AgendaMedico.xhtml?faces-redirect=true";
+				}else if (use.getIdPersona().getRoll().getId() == 4) {
+					Faces.setSessionAttribute("usuario", use);
+					Messages.addGlobalInfo("Usuario existe");
+
+					roles = seguridadEjb.listaRoles(useBuscar.getIdPersona().getRoll().getId());
+					accesos = seguridadEjb.listaAcc(useBuscar.getIdPersona().getRoll().getId());
+
+					return "/paginas/seguro/Farmaceuta/#.xhtml?faces-redirect=true";
+				}
 			} else {
 				Messages.addGlobalError("contrasena incorrecta");
 			}
@@ -109,24 +127,23 @@ public class SessionController implements Serializable {
 		return null;
 
 	}
-	
+
 	/**
 	 * Cerrar session
+	 * 
 	 * @return
 	 */
-	public String cerrarSession(){
-		usuario=null;
+	public String cerrarSession() {
+		usuario = null;
 		HttpSession sesion;
-		sesion=(HttpSession)Faces.getSession();
+		sesion = (HttpSession) Faces.getSession();
 		sesion.invalidate();
 		return "/paginas/publico/login.xhtml?faces-redirect=true";
 
 	}
-	
-	public boolean isSesion(){
-		return use!=null;
+
+	public boolean isSesion() {
+		return use != null;
 	}
-
-
 
 }
